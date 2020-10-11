5c3d896370c708b38b2fc1b174aca0be89545f03
Wagner Buzinari <wagner_mba@hotmail.com>
2020-10-11 09:41:53 -0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,25 +19,9 @@ export class AppComponent implements OnInit {
       }
     });
 
-    $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
-      if (!$(this).next().hasClass('show')) {
-        $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
-      }
-      var $subMenu = $(this).next('.dropdown-menu');
-      $subMenu.toggleClass('show');
-    
-    
-      $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-        $('.dropdown-submenu .show').removeClass('show');
-      });
-    
-    
-      return false;
-    });
-
-    if (location.protocol === 'http:') {
-      window.location.href = location.href.replace('http', 'https');
-    }
+    // if (location.protocol === 'http:') {
+    //   window.location.href = location.href.replace('http', 'https');
+    // }
   }
 
   ngAfterViewInit() {
